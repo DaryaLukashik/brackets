2b7e575ebed1b1132318678d3d932dca8c7d5570
DaryaLukashik <darya.ahmadova@gmail.com>
2023-02-27 11:52:02 +0300

    feat: solve brackets problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let res;
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    console.log(
+      "str=",
+      str,
+      bracketsConfig[i],
+      "after",
+      str.split(bracketsConfig[i][0])
+    );
+    console.log(
+      "str=",
+      str,
+      bracketsConfig[i],
+      "after",
+      str.split(bracketsConfig[i][0])
+    );
+    res =
+      str.split(bracketsConfig[i][0]).length ==
+      str.split(bracketsConfig[i][0]).length;
+    if (res == false) {
+      return false;
+    }
+  }
+  return res;
+};
